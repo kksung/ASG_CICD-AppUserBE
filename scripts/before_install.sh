dd211fa85687104a8c17b0beccac5a7a32ba9266
kksung <ki6sung@naver.com>
2024-01-18 15:15:49 +0900

    for ASG

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# codedeploy-agent 재시작
+sudo systemctl restart codedeploy-agent
+
 # 이전에 실행된 gunicorn 프로세스를 중지합니다.
 pkill -f 'gunicorn --bind 0.0.0.0:5000 --timeout 90 app:create_app()'
 
